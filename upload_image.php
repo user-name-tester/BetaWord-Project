619069d308803edc0d78aaf9d17367a30497b41a
Usernametest1000 <username1000@tutamail.com>
2025-04-21 00:16:38 +0000

    Update upload_image.php
    update upload_image.php

--- upload_image.php
+++ upload_image.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 header('Content-Type: application/json; charset=utf-8');
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
 header('Cache-Control: post-check=0, pre-check=0', false);
